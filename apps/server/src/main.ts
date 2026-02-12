c0587f3e6f73121dcb16f3ae284461cc32e0e1ad
Sro01 <sara0099@naver.com>
2026-02-13 01:22:56 +0900

    fix(server): CORS 에러 수정

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -8,6 +8,11 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
